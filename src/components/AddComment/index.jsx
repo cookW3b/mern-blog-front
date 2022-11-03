030d2b3bbc1ccba579d719d814d8e2659227f67f
cookW3b <artem.04.as@mail.ru>
2022-11-03 11:55:19 +0300

    create comments

--- src/components/AddComment/index.jsx
+++ src/components/AddComment/index.jsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 
 import styles from "./AddComment.module.scss";
 
 import TextField from "@mui/material/TextField";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
+import axios from '../../axios';
 
-export const Index = () => {
+export const Index = ({id, image}) => {
+
+	const [text, setText] = useState('');
+
+	const onSubmit = () => {
+		if(text === '') return;
+		const req = {
+			text
+		}
+		try {
+			axios.post(`/comments/${id}`, req)
+		} catch (error) {
+			alert('Не удалось оставить комментарий');
+		}
+	}
+	console.log(text)
+
   return (
     <>
       <div className={styles.root}>
         <Avatar
           classes={{ root: styles.avatar }}
-          src="https://mui.com/static/images/avatar/5.jpg"
+          src={image}
         />
         <div className={styles.form}>
           <TextField
@@ -21,8 +38,10 @@ export const Index = () => {
             maxRows={10}
             multiline
             fullWidth
+						value={text}
+						onChange={(e) => setText(e.target.value)}
           />
-          <Button variant="contained">Отправить</Button>
+          <Button onClick={onSubmit} variant="contained">Отправить</Button>
         </div>
       </div>
     </>
--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -27,7 +27,7 @@ export const FullPost = () => {
 				alert('Не удалось получиь статью');
 		});
 
-	}, [])
+	}, [comments])
 	if(isLoading) {
 		return <Post isLoading={isLoading} isFullPost />
 	}
@@ -59,27 +59,9 @@ export const FullPost = () => {
 						text: item.text
 					}
 				})}
-
-
-			// [
-			// 	{
-			// 		user: {
-			// 			fullName: "Вася Пупкин",
-			// 			avatarUrl: "https://mui.com/static/images/avatar/1.jpg",
-			// 		},
-			// 		text: "Это тестовый комментарий 555555",
-			// 	},
-			// 	{
-			// 		user: {
-			// 			fullName: "Иван Иванов",
-			// 			avatarUrl: "https://mui.com/static/images/avatar/2.jpg",
-			// 		},
-			// 		text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top",
-			// 	},
-			// ]
         isLoading={isLoading}
       >
-        <Index />
+        <Index id={id} image={data.user.avatarUrl} />
       </CommentsBlock>
     </>
   );
